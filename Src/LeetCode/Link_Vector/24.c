1fd6fcc85b547578a137cfcdadeb6611db9121be
JK-Lix <776579471@qq.com>
2019-04-11 18:47:15 +0800

    add a file

---
+++ Src/LeetCode/Link_Vector/24.c
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: 24.c
+	> Author: Lix
+	> Mail: 776579471@qq.com
+	> URL: https://github.com/JK-Lix
+	> Created Time: 2019年04月11日 星期四 18时40分56秒
+ ************************************************************************/
+#include<stdio.h>
+#include<stdlib.h>
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+
+struct ListNode* swapPairs(struct ListNode* head) {
+    
+}
+
+int main() {
+    printf("24.c\n");
+    return 0;
+}
+mZ
