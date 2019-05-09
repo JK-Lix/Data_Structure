483ffaa1f7d895f8fe3184973437614d3f773df0
JK-Lix <776579471@qq.com>
2019-05-09 20:07:30 +0800

    add a file

---
+++ Data/Search查找/triplesearch.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: triplesearch.cpp
+	> Author: Lix
+	> Mail: 776579471@qq.com
+	> URL: https://github.com/JK-Lix
+	> Created Time: 2019年05月07日 星期二 21时01分48秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//求解区间极值
+
+int main() {
+
+    return 0;
+}
+
