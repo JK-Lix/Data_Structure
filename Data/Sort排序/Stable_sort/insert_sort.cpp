278a01153d66dc858b75d6b7d316e59bbf6c379b
JK-Lix <776579471@qq.com>
2019-05-09 20:07:46 +0800

    add a file

---
+++ Data/Sort排序/Stable_sort/insert_sort.cpp
@@ -0,0 +1,61 @@
+/*************************************************************************
+	> File Name: insert_sort.cpp
+	> Author: Lix
+	> Mail: 776579471@qq.com
+	> URL: https://github.com/JK-Lix
+	> Created Time: 2019年05月07日 星期二 20时34分06秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+#define swap(a, b) {\
+    a ^= b; b ^= a; a ^= b;\
+}
+
+#define TEST(arr, n, func, args...) { \
+    int *num = (int *)malloc(sizeof(int) * n); \
+    memcpy(num, arr, sizeof(int) * n); \
+    output(num, n); \
+    printf("%s = ", #func); \
+    func(args); \
+    output(num, n); \
+    free(num); \
+}
+
+void insert_sort(int *num, int n) {
+    for (int i = 1; i < n; i++) {
+        for (int j = i; j > 0 && num[j] < num[j - 1]; --j) {
+            swap(num[j], num[j - 1]);
+        }
+    }
+    return;
+}
+
+void randint(int *num, int n) {
+    while (n--) num[n] = rand() % 100;
+    return ;
+}
+
+void output(int *num, int n) {
+    printf("[");
+    for (int i = 0; i < n; i++) {
+        printf(" %d", num[i]);
+    }
+    printf("]\n");
+    return ;
+}
+
+int main() {
+    srand(time(0));
+    #define MAX_OP 20
+    int arr[MAX_OP];
+    randint(arr, MAX_OP);
+    TEST(arr, MAX_OP, insert_sort, num, MAX_OP);
+    return 0;
+}
+
---
+++ Data/Sort排序/Unstable_sort/select_sort.cpp
@@ -0,0 +1,62 @@
+/*************************************************************************
+	> File Name: select_sort.cpp
+	> Author: Lix
+	> Mail: 776579471@qq.com
+	> URL: https://github.com/JK-Lix
+	> Created Time: 2019年05月07日 星期二 20时56分46秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define swap(a, b) { \
+    __typeof(a) _temp = a; a = b; b = _temp; \
+}
+
+#define TEST(arr, n, func, args...) { \
+    int *num = (int *)malloc(sizeof(int) * n); \
+    memcpy(num, arr, sizeof(int) * n); \
+    output(num, n); \
+    printf("%s = ", #func); \
+    func(args); \
+    output(num, n); \
+    free(num); \
+}
+
+void select_sort(int *num, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int ind = i;
+        for (int j = i + 1; j < n; j++) {
+            if (num[ind] > num[j]) ind = j;
+        }
+        swap(num[i], num[ind]);
+    }
+    return ;
+}
+
+void randint(int *num, int n) {
+    while (n--) num[n] = rand() % 100;
+    return ;
+}
+
+void output(int *num, int n) {
+    printf("[");
+    for (int i = 0; i < n; i++) {
+        printf(" %d", num[i]);
+    }
+    printf("]\n");
+    return ;
+}
+
+int main() {
+    srand(time(0));
+    #define MAX_OP 20
+    int arr[MAX_OP];
+    randint(arr, MAX_OP);
+    TEST(arr, MAX_OP, select_sort, num, MAX_OP);
+    return 0;
+}
+
